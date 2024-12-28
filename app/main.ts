d312fafbad367ae63db7c20fac2bedc737ae5438
Kulakov Yuriy Andreevich <ykulakov@ozon.ru>
2024-12-28 20:11:12 +0300

    extract correlation_id

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,16 @@
 import net from "node:net";
 
 const server = net.createServer((connection) => {
+  // Handle request message
   connection.on("data", (req) => {
+    // get int32 with offset 8 bytes
+    const correlation_id = req.buffer.slice(8, 13);
+
+    // response message
     const buffer = Buffer.alloc(8);
-    buffer.writeInt32BE(0, 0);
-    buffer.writeInt32BE(7, 4);
+
+    buffer.writeInt32BE(0, 0); // message_size
+    buffer.fill(Buffer.from(correlation_id), 4);
 
     connection.write(buffer);
   });
