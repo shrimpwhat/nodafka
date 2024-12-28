97d4ac5feaeba0b49c1d5007e0ce9d2bf7b8040c
Kulakov Yuriy Andreevich <ykulakov@ozon.ru>
2024-12-28 20:01:24 +0300

    correlation_id

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,13 @@
 import net from "node:net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
+  connection.on("data", (req) => {
+    const buffer = Buffer.alloc(8);
+    buffer.writeInt32BE(0, 0);
+    buffer.writeInt32BE(7, 4);
+
+    connection.write(buffer);
+  });
 });
 
 server.listen(9092, "127.0.0.1");
